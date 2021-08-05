87824774ed912f7c15df43175ac9f30ece3603b7
Fabian Blum <fabian.blum@hotmail.com>
2021-08-05 23:20:04 +0200

    Current status, nearly RC version

---
+++ UI/Controls/Game/PvEProtection/Data/ViewModelHUDPvEProtectionInfo.cs
@@ -0,0 +1,43 @@
+﻿namespace AtomicTorch.CBND.CoreMod.UI.Controls.Game.PvEProtection.Data
+{
+    using System.Windows;
+    using AtomicTorch.CBND.CoreMod.Helpers.Client;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Core;
+
+    public class ViewModelHUDPvEProtectionInfo : BaseViewModel
+    {
+        private double timeRemaining;
+
+        public ViewModelHUDPvEProtectionInfo()
+        {
+        }
+
+        public string ProtectionTimeRemainingText { get; set; }
+
+        public float RequiredHeight { get; set; }
+
+        public Visibility Visibility { get; private set; }
+
+        public void Setup(double timeRemaining)
+        {
+        }
+
+        protected override void DisposeViewModel()
+        {
+            base.DisposeViewModel();
+        }
+
+        private void UpdateText()
+        {
+            if (this.IsDisposed)
+            {
+                return;
+            }
+        }
+
+        private void UpdateTimerOnly()
+        {
+            this.Visibility = Visibility.Visible;
+        }
+    }
+}
---
+++ UI/Controls/Game/PvEProtection/HUDPvEProtectionInfo.xaml.cs
@@ -0,0 +1,78 @@
+﻿namespace AtomicTorch.CBND.CoreMod.UI.Controls.Game.PvEProtection
+{
+    using System.Windows;
+    using System.Windows.Input;
+    using AtomicTorch.CBND.CoreMod.Systems.PvEProtection;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Core.Menu;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Game.PvEProtection.Data;
+    using AtomicTorch.CBND.CoreMod.UI.Controls.Game.Social;
+    using AtomicTorch.GameEngine.Common.Client.MonoGame.UI;
+
+    public partial class HUDPvEProtectionInfo : BaseUserControl
+    {
+        private const double ExpandOrCollapseDelay = 0.1;
+
+        private int expandedStateRefreshScheduledNumber;
+
+        private FrameworkElement layoutRoot;
+
+        private ViewModelHUDPvEProtectionInfo viewModel;
+
+        protected override void InitControl()
+        {
+            this.layoutRoot = this.GetByName<FrameworkElement>("LayoutRoot");
+        }
+
+        protected override void OnLoaded()
+        {
+            this.UpdateLayout();
+
+            this.viewModel = new ViewModelHUDPvEProtectionInfo();
+            this.viewModel.RequiredHeight = (float)this.GetByName<FrameworkElement>("Description")
+                                                       .ActualHeight;
+            this.DataContext = this.viewModel;
+
+            this.expandedStateRefreshScheduledNumber = 0;
+
+            VisualStateManager.GoToElementState(this.layoutRoot,
+                                                "Collapsed",
+                                                useTransitions: false);
+
+            this.MouseEnter += this.MouseEnterOrLeaveHandler;
+            this.MouseLeave += this.MouseEnterOrLeaveHandler;
+            this.MouseDown += MouseDownHandler;
+        }
+
+        protected override void OnUnloaded()
+        {
+            this.DataContext = null;
+            this.viewModel.Dispose();
+            this.viewModel = null;
+        }
+
+        private static void MouseDownHandler(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void MouseEnterOrLeaveHandler(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void NewbieProtectionTimeRemainingReceivedHandler(double obj)
+        {
+
+        }
+
+        private void RefreshState(int refreshNumber)
+        {
+
+        }
+
+        private void RefreshTimeRemaining()
+        {
+
+        }
+    }
+}
